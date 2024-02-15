613a6f46200459139530d6a0e4a4121b5a7bca71
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2024-02-15 19:24:42 +0530

    dialog-box-added

--- src/component/DialogBox/DialogBox.jsx
+++ src/component/DialogBox/DialogBox.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Button, Dialog, Portal, PaperProvider, Text } from 'react-native-paper';
+import KeyboardInput from '../../common/KeyboardInput/KeyboardInput';
+import MyButton from '../../common/Button/Button';
 
 export default function DialogBox() {
 
@@ -14,16 +16,61 @@ export default function DialogBox() {
                 <Button onPress={showDialog}>Show Dialog</Button>
                 <Portal>
                     <Dialog visible={visible} onDismiss={hideDialog}>
-                        <Dialog.Title>Alert</Dialog.Title>
-                        <Dialog.Content>
-                            <Text variant="bodyMedium">This is simple dialog</Text>
-                        </Dialog.Content>
-                        <Dialog.Actions>
-                            <Button onPress={hideDialog}>Done</Button>
-                        </Dialog.Actions>
+                        <Dialog.Title>Update Student !</Dialog.Title>
+                        <View style={{ padding: 20 }}>
+                            <KeyboardInput
+                                label={"Name"}
+                                style={{ marginBottom: 10 }}
+                            />
+
+                            <KeyboardInput
+                                label={"Age"}
+                                style={{ marginBottom: 10 }}
+                            />
+
+                            <KeyboardInput
+                                label={"Address"}
+                                style={{ marginBottom: 10 }}
+                            />
+
+                            <KeyboardInput
+                                label={"Contact"}
+                                style={{ marginBottom: 10 }}
+                            />
+
+                        </View>
+
+
+
+                        <View style={{ padding: 20 }}>
+                            <MyButton
+                                text={"Update"}
+                                buttonColor={'#16a085'}
+                                textColor={'white'}
+                                rippleColor={"#B1DFD6"}
+                                onPress={hideDialog}
+                                style={styles.btn}
+                            />
+
+                            <MyButton
+                                text={"Cansel"}
+                                buttonColor={'#c0392b'}
+                                textColor={'white'}
+                                rippleColor={"#EABDB8"}
+                                onPress={hideDialog}
+                                style={styles.btn}
+                            />
+                        </View>
                     </Dialog>
                 </Portal>
             </View>
         </PaperProvider>
     );
 };
+
+const styles = StyleSheet.create({
+    btn: {
+        borderRadius: 8,
+        marginBottom: 10
+    },
+})
